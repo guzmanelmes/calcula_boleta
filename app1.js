ec7903eb9df9118587fe1b70d3730509e5775fe4
Rodrigo Guzman <rguzman1@hotmail.cl>
2020-04-21 18:52:26 -0400

    Archivo Index y app version 4

--- app1.js
+++ app1.js
@@ -2,27 +2,18 @@ function myFunction() {
     var x, y, suma, text;
     valor_pactado = document.getElementById("valor_pactado").value;
 
-    valor_boleta_liquido = valor_pactado;
-    valor_boleta_bruto = valor
+    valor_boleta_liquido = parseInt(valor_pactado) / 0.8925;
+    valor_boleta_bruto = parseInt(valor_pactado);
+    valor_retencion_liquido = parseInt(valor_boleta_liquido) * 0.1075;
+    valor_retencion_bruto = parseInt(valor_boleta_bruto) * 0.1075;
 
-    valor_retencion_liquido
-    valor_retencion_bruto
+    valor_efectivo_liquido = parseInt(valor_pactado);
+    valor_efectivo_bruto = parseFloat(valor_pactado) + valor_retencion_bruto;
 
-    valor_efectivo_liquido
-    valor_efectivo_bruto
-
-    if (isNaN(x) || isNaN(y)) {
-        text = "Es necesarios introducir dos números válidos";
-    } else {
-        //si no ponemos parseFloat concatenaría x con y  
-        suma = parseFloat(x) + parseFloat(y);
-        text = suma;
-
-    }
-    document.getElementById("valor_boleta_liquido").innerHTML = valor_boleta_liquido;
-    document.getElementById("valor_boleta_bruto").innerHTML = valor_boleta_bruto;
-    document.getElementById("valor_retencion_liquido").innerHTML = valor_retencion_liquido;
-    document.getElementById("valor_rentencion_bruto").innerHTML = valor_efectivo_bruto;
-    document.getElementById("valor_efectivo_liquido").innerHTML = valor_efectivo_liquido;
-    document.getElementById("valor_efectivo_bruto").innerHTML = valor_efectivo_bruto;
+    document.getElementById("valor_boleta_liquido").innerHTML = new Intl.NumberFormat("de-DE").format(valor_boleta_liquido.toFixed(0));
+    document.getElementById("valor_boleta_bruto").innerHTML = new Intl.NumberFormat("de-DE").format(valor_boleta_bruto.toFixed(0));
+    document.getElementById("valor_retencion_liquido").innerHTML = new Intl.NumberFormat("de-DE").format(valor_retencion_liquido.toFixed(0));
+    document.getElementById("valor_retencion_bruto").innerHTML = new Intl.NumberFormat("de-DE").format(valor_retencion_bruto.toFixed(0));
+    document.getElementById("valor_efectivo_liquido").innerHTML = new Intl.NumberFormat("de-DE").format(valor_efectivo_liquido.toFixed(0));
+    document.getElementById("valor_efectivo_bruto").innerHTML = new Intl.NumberFormat("de-DE").format(valor_efectivo_bruto.toFixed(0));
 }
